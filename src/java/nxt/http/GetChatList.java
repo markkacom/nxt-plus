2beb3ef6ba68e71365b031907795709798a2e06e
Dirk <incentivetoken@gmail.com>
2015-08-01 09:46:06 +0200

    GetChatList and GetChatMessages API

---
+++ src/java/nxt/http/GetChatList.java
@@ -0,0 +1,60 @@
+package nxt.http;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import nxt.Account;
+import nxt.Account.AccountInfo;
+import nxt.MofoChat;
+import nxt.MofoChat.Chat;
+import nxt.util.Convert;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONStreamAware;
+
+public class GetChatList extends APIServlet.APIRequestHandler{
+
+    static final GetChatList instance = new GetChatList();
+
+    private GetChatList() {
+        super(new APITag[] {APITag.MESSAGES}, "account", "firstIndex", "lastIndex");
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    JSONStreamAware processRequest(HttpServletRequest req) throws ParameterException {
+
+        Account account = ParameterParser.getAccount(req);
+        int firstIndex = ParameterParser.getFirstIndex(req);
+        int lastIndex = ParameterParser.getLastIndex(req);
+        
+        List<Chat> chatList = MofoChat.getChatList(account.getId(), firstIndex, lastIndex);
+        JSONArray chats = new JSONArray();
+
+        for (Chat c : chatList) {
+            JSONObject chat = new JSONObject();
+            chat.put("accountRS", Convert.rsAccount(c.getAccountId()));
+            Account other = Account.getAccount(c.getAccountId());
+            if (other != null) {
+                AccountInfo info = other.getAccountInfo();
+                if (info != null) {
+                    chat.put("accountName", info.getName());
+                }
+            }
+            chat.put("timestamp", c.getTimestamp());
+            chats.add(chat);
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("accountRS",  Convert.rsAccount(account.getId()));
+        
+        AccountInfo info = account.getAccountInfo();
+        if (info != null) {
+            response.put("accountName", info.getName());
+        }
+        response.put("chats", chats);
+        return response;
+    }
+}
